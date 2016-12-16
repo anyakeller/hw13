ade8883d8587c162b40c851053d453bc271d2d45
JFreyra <efreyra@stuy.edu>
2016-12-16 13:06:00 -0500

    added fibonacci and e equations

--- intro.js
+++ intro.js
@@ -1,76 +1,3 @@
-
-/* notes 'n stuff
-//send diagnostic output to console
-//(Ctrl-Shift-J in Chromium & Firefox to reveal console)
-console.log("AYO");
-
-var i = "hello";
-var j = 20;
-
-
-//assign an anonymous fxn to a var
-var f = function(x) {
-    var j=30;
-    return j+x;
-};
-
-
-//(define fact (lambda (n) ...)
-var fact = function(n) {
-    var prod=1;
-    for ( ; n > 1 ; n--){
-    prod = prod * n;
-    }
-    return prod;
-};
-
-
-//(define fact (lambda (n) ...)
-var factR = function(n) {
-    if ( n<=1 ) {
-    return 1;
-    }
-    else {
-    return n * factR(n-1);
-    }
-};
-
-
-//add list item 
-var addItem = function(text) {
-    var list = document.getElementById("thelist");
-    var newitem = document.createElement("li");
-    newitem.innerHTML = text;
-    list.appendChild(newitem);
-};
-
-
-//remove specified item from list
-var removeItem = function(n) {
-    var listitems = document.getElementsByTagName('li');
-    listitems[n].remove();
-};
-
-
-var red = function() {
-    var items = document.getElementsByTagName("li");
-    for(var i = 0; i < items.length; i++) {
-    items[i].classList.add('red');
-    }
-};
-
-
-//instantiate an object
-var o = { 'name' : 'Thluffy',
-      age : 15,
-      items : [10, 20, 30, 40],
-      morestuff : {a : 1, b : 'ayo'},
-      func : function(x) {
-          return x+30;
-      }
-    };
-    */
-
 //current number
 var currnum = 8;
 var l = document.getElementById('thelist');
@@ -96,13 +23,13 @@ var removeItem = function() {
 
 //change head to list element when mouseover
 var listHead = function(e) {
-    console.log(this);
+    //console.log(this);
     document.getElementById("h").innerHTML = this.innerHTML;
 };
 
 //change head back to Hello World! when mouseout
 var returnHead = function(e) {
-    console.log(this);
+    //console.log(this);
     document.getElementById("h").innerHTML = "Hello World!";
 };
 
@@ -113,4 +40,48 @@ for(var i = 0; i<li.length; i++) {
     li[i].addEventListener('mouseover',listHead);
     li[i].addEventListener('mouseout',returnHead);
     li[i].addEventListener('click',removeItem);
-}
+};
+
+var fibby = 1;
+var prefibby = 0;
+var addFib = function() {
+    var list = document.getElementById("fiblist");
+    var newitem = document.createElement("li");
+    newitem.innerHTML = fibby;
+    var temp = fibby;
+    fibby = fibby + prefibby;
+    prefibby = temp;
+    list.appendChild(newitem);
+
+    newitem.addEventListener('mouseover',listHead);
+    newitem.addEventListener('mouseout',returnHead);
+};
+
+fib.addEventListener('click',addFib);
+
+var fact = function(n){
+    var ans = 1;
+    while(n != 0){
+	ans = ans * n;
+	n = n-1;
+    };
+    return ans;
+};
+
+var esum = 0;
+var retE = function(n) {
+    esum = esum + (1.0 / fact(n));
+    return esum;
+};
+
+var addE = function() {
+    var list = document.getElementById("elist");
+    var newitem = document.createElement("li");
+    newitem.innerHTML = retE(list.getElementsByTagName("li").length);
+    list.appendChild(newitem);
+
+    newitem.addEventListener('mouseover',listHead);
+    newitem.addEventListener('mouseout',returnHead);
+};
+
+eh.addEventListener('click',addE);
